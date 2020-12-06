384fbfde6c9944edc69ee456783ebca6ee271db4
Rizqi Mauludin <40969099+rizqimauludin10@users.noreply.github.com>
2020-12-06 22:24:13 +0700

    Update tugaskalkulator.cpp
    Nope

--- tugaskalkulator.cpp
+++ tugaskalkulator.cpp
@@ -1,17 +1,85 @@
 #include <iostream>
 #include <string>
 #include <conio.h>
+#include <stdlib.h>
+#include <windows.h>
 
 using namespace std;
+
+const int BLUE = 1;
+const int GREEN = 2;
+const int RED = 4;
+const int WHITE = 15;
 
 //Fungsi nama kelompok
 void namaKelompok()
 {
-    cout << "Ini adalah sebuah fungsi\n";
+    // system("Color E4");
+    cout << "Nama Kelompok\n";
+    cout << "1. Rizka Nur Remadiani\n";
+    cout << "2. Putri Handayani\n";
+    cout << "3. Siti Sarah\n";
+    cout << "4. Khairunnisa Amir\n";
+    cout << "\n\n";
+    cout << "------------------------\n";
+}
+
+void clearScreen(int characterLength)
+{
+    for (int i = 0; i < characterLength; i++)
+    {
+        cout << "\b";
+    }
+}
+
+void changeColour(int colour)
+{
+    HANDLE hConsole;
+    hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+    SetConsoleTextAttribute(hConsole, colour);
+}
+
+void showLoadingScreen()
+{
+    int i;
+    string closed = "---", open = "***";
+    int colour[] = {RED, GREEN, BLUE};
+
+    cout << closed;
+
+    for (i = 0; i < 3; i++)
+    {
+        Sleep(1000);
+        clearScreen(5);
+        changeColour(colour[i]);
+
+        cout << open;
+
+        Sleep(1000);
+        clearScreen(5);
+        changeColour(WHITE);
+    }
+}
+
+void showTitle()
+{
+    changeColour(RED);
+    cout << "Madrasah Aliyah\n";
+    Sleep(1000);
+
+    changeColour(GREEN);
+    cout << "AL-MUSTAQIM\n";
+    Sleep(1000);
+
+    changeColour(WHITE);
+    cout << "TUGAS TIK\n\n";
+    Sleep(1000);
 }
 
 int main()
 {
+    showLoadingScreen();
+    showTitle();
     namaKelompok();
 
     // Deklarasi Variabel
@@ -22,13 +90,13 @@ int main()
 
 // Fungsi menu atau pilihan operator
 menu:
-    cout << "Silahkan Pilih Operator" << endl;
-    cout << "1. Penjumlahan" << endl;
-    cout << "2. Pengurangan" << endl;
-    cout << "3. Perkalian" << endl;
-    cout << "4. Pembagian" << endl;
-    cout << "5. Modulus" << endl;
-    cout << endl;
+    cout << "Silahkan Pilih Operator\n";
+    cout << "1. Penjumlahan\n";
+    cout << "2. Pengurangan\n";
+    cout << "3. Perkalian\n";
+    cout << "4. Pembagian\n";
+    cout << "5. Modulus\n";
+    cout << "\n";
 
     cout << "Masukan Pilihan : ";
     cin >> pil;
@@ -60,12 +128,13 @@ menu:
         operasi = '%';
         break;
     default:
-        cout << "Salah Memasukkan Operator" << endl;
+        cout << "Salah Memasukkan Operator\n";
     }
 
-    cout << "*----------------------*" << endl;
-    cout << "    " << bil1 << operasi << bil2 << "=" << hasil << endl;
-    cout << "*----------------------*" << endl;
+    cout << "*----------------------*\n";
+    cout << "    " << bil1 << operasi << bil2 << "=" << hasil << "\n";
+    cout << "*----------------------*"
+         << "\n";
 
     cout << "Coba Lagi? (y atau t) : ";
     cin >> lagi;
